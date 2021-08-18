ca0f5ab2504f930cdba6e0cb5229be3c6391bc17
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-08-18 20:45:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,15 +12,47 @@
 		nav.classList.toggle('visible');
 	}
 
-  //setting up departments card shuffle effect
-  let cards = [].slice.call(document.querySelectorAll('.card'));
-  let cardW = document.querySelector('#cardW');
-  
-	//setting up the clicked Effect
-  cardW.addEventListener('click', function () {
-  	cards.forEach(function (card) {
-  		card.classList.add('transformed')
-  	});
-  	cardW.classList.add('hidden');
+  	//setting up departments card shuffle and unshuffle effect
+ 	 let cards = [].slice.call(document.querySelectorAll('.card'));
+ 	 let cardW = document.querySelector('#cardW');
+ 	 let cardL = document.querySelector('#cardL');
 
-  });
+	//setting up the card shuffling effect
+  	cardW.addEventListener('click', function () {
+  		cards.forEach(function (card) {
+  			card.classList.add('transformed')
+  		});
+  		cardW.classList.add('hidden');
+ 	 });
+
+	// Setting up the card unshuffling effect
+	cardL.addEventListener('click', function () {
+		cards.forEach(function (card) {
+			card.classList.remove('transformed')
+		});
+		cardW.classList.remove('hidden');
+	});
+
+
+	//Back to top button function
+
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.querySelector(".back-to-top").style.transform = "translateY(0px)";
+			document.querySelector(".back-to-top").style.opacity = "1";
+		} else {
+			document.querySelector(".back-to-top").style.transform = "translateY(1000px)";
+			document.querySelector(".back-to-top").style.opacity = "0";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+

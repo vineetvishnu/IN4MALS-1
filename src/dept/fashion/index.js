a0464aba607f1e0c0c6ea54db54f05e86184cfd0
swayam verma <61063154+swayamverma412@users.noreply.github.com>
2021-08-24 10:35:56 +0530

    About us

--- src/dept/fashion/index.js
+++ src/dept/fashion/index.js
@@ -11,3 +11,18 @@ var bars = document.getElementById("nav-action");
 		bars.classList.toggle('active');
 		nav.classList.toggle('visible');
 	}
+	//Back to top button function
+
+	window.onscroll = function () {
+		scrollFunction()
+	};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			document.querySelector(".back-to-top").style.transform = "translateY(0px)";
+			document.querySelector(".back-to-top").style.opacity = "1";
+		} else {
+			document.querySelector(".back-to-top").style.transform = "translateY(1000px)";
+			document.querySelector(".back-to-top").style.opacity = "0";
+		}
+	}
